d1ed2606203528dd3fb207d196f6508b5d9cff0c
Diego Dias <diego2007jf@gmail.com>
2023-06-13 17:02:32 -0300

    removed wrong import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GameScreenComponent } from './components/game/game-screen/game-screen.component';
 import { GameComponent } from './components/game/game.component';
 import { ScreenComponent } from './components/game/screen/screen.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GameScreenComponent,
     GameComponent,
     ScreenComponent
   ],
