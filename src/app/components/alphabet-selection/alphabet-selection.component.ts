ce7ab9cd3d35221c1d177958016cd5170bfc560a
Diego Dias <diego2007jf@gmail.com>
2023-06-19 13:48:52 -0300

    core functionality working

--- src/app/components/alphabet-selection/alphabet-selection.component.ts
+++ src/app/components/alphabet-selection/alphabet-selection.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-alphabet-selection',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./alphabet-selection.component.css']
 })
 export class AlphabetSelectionComponent {
+  constructor(private router: Router) {}
 
+  navigateToLettersSelection(alphabet: string) {
+    this.router.navigate(['/letters-selection'], { queryParams: { alphabet } });
+  }
 }
