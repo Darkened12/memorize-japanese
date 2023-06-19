73de4964809381e4f473ba375286f78fbe331931
Diego Dias <diego2007jf@gmail.com>
2023-06-19 13:26:59 -0300

    added katakana support

--- src/app/services/alphabet.service.ts
+++ src/app/services/alphabet.service.ts
@@ -1,17 +1,25 @@
 import { Injectable } from '@angular/core';
 import { JapaneseAlphabet } from '../models/japanese-alphabet.model';
 import hiragana from '../datasets/hiragana.dataset';
+import katakana from '../datasets/katakana.dataset';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AlphabetService {
-  japaneseLetters: JapaneseAlphabet[] = hiragana;
 
-  constructor() { }
+  getjapaneseLetters(alphabet: string, lettersToInclude: string[]): JapaneseAlphabet[] {
+    var desiredAlphabet: JapaneseAlphabet[];
 
-  getjapaneseLetters(lettersToInclude: string[]): JapaneseAlphabet[] {
-    return this.japaneseLetters.filter(letter => lettersToInclude.includes(letter.type));
+    if (alphabet === 'hiragana') {
+      desiredAlphabet = hiragana;
+    }
+    else if (alphabet === 'katakana') {
+      desiredAlphabet = katakana;
+    }
+    else { throw new Error('desiredAlphabet is not set') }
+    
+    return desiredAlphabet.filter(letter => lettersToInclude.includes(letter.type));
   }
   
 
