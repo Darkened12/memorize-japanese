2d3fd7c0ac981040e5857f41822cffd2fd344076
Diego Dias <diego2007jf@gmail.com>
2023-06-19 13:34:32 -0300

    updated paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GameComponent } from './components/game/game.component';
 import { LettersSelectionComponent } from './components/letters-selection/letters-selection.component';
+import { AlphabetSelectionComponent } from './components/alphabet-selection/alphabet-selection.component';
 
 const routes: Routes = [
-  {path: '', component: LettersSelectionComponent},
+  {path: '', component: AlphabetSelectionComponent},
+  {path: 'letters-selection', component: LettersSelectionComponent},
   {path: 'game', component: GameComponent}
 ];
 
