b3e2b8cd084c907df7df04f87e324892c29480eb
Diego Dias <diego2007jf@gmail.com>
2023-06-19 14:01:26 -0300

    added routing functionality

--- src/app/components/letters-selection/letters-selection.component.ts
+++ src/app/components/letters-selection/letters-selection.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { JapaneseAlphabet } from 'src/app/models/japanese-alphabet.model';
 import hiragana from 'src/app/datasets/hiragana.dataset';
+import { BehaviorSubject } from 'rxjs';
+import katakana from 'src/app/datasets/katakana.dataset';
 
 @Component({
   selector: 'app-letters-selection',
@@ -9,15 +11,35 @@ import hiragana from 'src/app/datasets/hiragana.dataset';
   styleUrls: ['./letters-selection.component.css']
 })
 export class LettersSelectionComponent {
+  alphabet$ = new BehaviorSubject<JapaneseAlphabet[]>([]);
   checkboxes: { id: string, value: string, label: string, checked: boolean }[] = [];
   selectAllChecked: boolean = false;
 
-  constructor(private router: Router) {
-    this.createCheckboxes();
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.activatedRoute.queryParams.subscribe(params => {
+      const alphabetName = params['alphabet'];
+      
+      if (alphabetName === 'hiragana') {
+        this.alphabet$.next(hiragana);
+      }
+
+      else if (alphabetName === 'katakana') {
+        this.alphabet$.next(katakana);
+      }
+      
+      else { 
+        this.router.navigate(['/']); 
+      }
+      
+      this.createCheckboxes();
+    });
   }
+  
 
   createCheckboxes() {
-    const uniqueLetters = Array.from(new Set(hiragana.map((letter: JapaneseAlphabet) => letter.type)));
+    const uniqueLetters = Array.from(new Set(this.alphabet$.getValue().map((letter: JapaneseAlphabet) => letter.type)));
   
     this.checkboxes = uniqueLetters.map((letter: string) => {
       return {
@@ -43,7 +65,11 @@ export class LettersSelectionComponent {
       .filter(checkbox => checkbox.checked)
       .map(checkbox => checkbox.value);
   
-    this.router.navigate(['/game'], { queryParams: { letters: checkedLetters } });
+    this.activatedRoute.queryParams.subscribe(params => {
+      this.router.navigate(
+          ['/game'], { queryParams: { alphabet: params['alphabet'], letters: checkedLetters } }
+        );
+    })
   }
   
 
