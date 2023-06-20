e23a9dbbe6d0c5e1b064eabfdbe482ee0b58370f
Diego Dias <diego2007jf@gmail.com>
2023-06-20 13:40:49 -0300

    updated app's name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.japaneselearning.app',
-  appName: 'japanese-learning',
+  appId: 'com.memorizejapanese.app',
+  appName: 'Hira/Kata Memorization',
   webDir: 'dist/japanese-learning',
   server: {
     androidScheme: 'https'
