13641a20155f133b601c377677305f8f2e7ae3a6
Diego Dias <diego2007jf@gmail.com>
2023-06-27 16:27:13 -0300

    created utils service

---
+++ src/app/services/utils.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UtilsService {
+  shuffleArray(array: []) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+}
